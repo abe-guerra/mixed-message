2d055e4fec2d16a915f02e91162d40d2f137f1cb
Abraham Guerra <94548421+abe-guerra@users.noreply.github.com>
2021-11-28 16:49:52 -0500

    Added BUILD insults

--- hello.js
+++ hello.js
@@ -1,3 +1,42 @@
+// Random insult Generator (I don't like your [element])
+
+
+//If its a BUILD characteristic 
+let buildInsult = [
+'plump',
+'chubby',
+'stocky',
+'overweight',
+'fat',
+'pudgy',
+'medium build',
+'small frame',
+'large frame',
+'athletic build',
+'slender',
+'lanky',
+'fit',
+'slim',
+'trim',
+'skinny',
+'skeletal',
+'buff',
+'toned',
+'muscular',
+'ripped',
+'well built'
+]
+
+
+
+
+// If its a HEIGHT characteristic
+
+
+
+
+
+//
 let messages = []
 let randNum = Math.floor(Math.random() * messages.length + 1)
 
